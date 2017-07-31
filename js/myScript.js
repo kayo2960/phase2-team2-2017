e8782482f561840d359d5085de343ddd0eea55ae
kayo2960 <cayokayo@gmail.com>
2017-07-31 20:52:44 +0000

    update monday

--- js/myScript.js
+++ js/myScript.js
@@ -14,15 +14,15 @@ function initMap() {
  
       
 /*animation page example*/
-/* global$*/
+/*global$*/
 
 /* Target the item using the class as the selector.*/
 /* Single animation*/
-$('.box1').animate({left: 200}, 500);
-$('.box4').animate({left: 100}, 300);
+$('.box1').animate({left: 500}, 100);
+$('.box4').animate({left: 1000}, 300);
 /* Chained animation*/
 $('.box2').animate({ 
-    top: 100,
+    top: 70,
     left: 100,
     width: 100,
     height: 100,
@@ -83,33 +83,7 @@ $('.box3').animate({
 /*global currentPos*/
  /*global math*/
 /*///////////////////////////////////////////////////anime////////////////////////////*/
-
-/*var theThing = document.querySelector("#img1");
-var requestAnimationFrame = windows.requestAnimationFrame;
-
-function moveThing(){
-  currentPos +=5;
-  
-  theThing.style.left = currentPos + "px";
-    if(Math.abs(currentPos) >=900){
-      currentPos = -500;
-    }
-    
-    requestAnimationFrame(moveThing);
-}
-moveThing();*/
-
-/*--------parapara*/
-/*var c = 0;
-    function parapara(){
-        c++;
-        var no = c % 2;
-        document.getElementById('img1').src= no + ".png";
-    }
-    function startAnima() {
-        setInterval("parapara(id)",600);
-    }
-*/    
+   
 function myMove() {
     var elem = document.getElementById("img1"); 
     var pos = 0;
@@ -125,3 +99,30 @@ function myMove() {
     }
 }    
     
+
+
+/*////////////////////////form radio code////////////////////////////*/
+
+function myFunction() {
+    var x = document.getElementById("myRadio2").value;
+    document.getElementById("results").innerHTML = x;
+}
+
+function howFeels() {
+  var text;
+  var howFeels = document.getElementById("myRadio").value;
+
+  switch(howFeels) {
+    // add code here
+    case "very good":
+    text window.open(src="do.thml");
+    break;
+    case "very good":
+    text window.open(src="see.thml");
+    break;
+    case "very good":
+    text window.open(src="eat.html");
+    break;
+  }
+  document.getElementById("demo").innerHTML = text;
+}
