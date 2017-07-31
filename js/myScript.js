2f0c6390d1d7932ab1e2333d480faaa3d3099c54
kayo2960 <cayokayo@gmail.com>
2017-07-31 14:33:43 +0000

    latest up date 07-31-17

--- js/myScript.js
+++ js/myScript.js
@@ -1,47 +1,127 @@
 // JavaScript File
-    
-function changeImage() {
-var image = document.getElementById('myImage');
-  if (image.src.match("tuna")) {
-    image.src="img/tunas.png";
-  }else {
-    image.src="img/tuna.png";
- }
-}
+/* global$*/   
 
 
 /*global google map js*/
 function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: { lat: 40.730610, lng: -73.9352424 },
+          center: { lat: 40.781523, lng: -73.966675 },
           scrollwheel: false,
-          zoom: 8
+          zoom: 14
         });
       }
+ 
       
-// fish html
-$(document).ready(function(){
-  
- /* $(function(){
-	$("button#fadeIn").click(function(){
-		$("img:not(:animated)").fadeIn("slow");
-	});
-	$("button#fadeOut").click(function(){
-		$("img:not(:animated)").fadeOut("slow");
-	});
-});*/
-  
-    $('#slide').click(function(){
-        $('.slide-box').effect('slide',{direction: 'right'}, 'slow');
-    });
-    
-    $(".slide-box").click(function(){
-        $(this).effect('explode',{pieces: 18},'slow');
-    });
-    
-  /*$('#show-text').click(function(){
-        $('#hidden-text').fadeToggle('slow')
-    });*/
-   
+/*animation page example*/
+/* global$*/
+
+/* Target the item using the class as the selector.*/
+/* Single animation*/
+$('.box1').animate({left: 200}, 500);
+$('.box4').animate({left: 100}, 300);
+/* Chained animation*/
+$('.box2').animate({ 
+    top: 100,
+    left: 100,
+    width: 100,
+    height: 100,
+}).animate({
+    top: 50,
+    left: 150,
+    width: 50,
+    height: 50,
+}).animate({
+    top: 150,
+    left: 300,
+    width: 300,
+    height: 120,
+}).animate({
+    top: 70,
+    left: 10,
+    width: 50,
+    height: 50,
+}).animate({
+    top: 350,
+    left: 10,
+    width: 50,
+    height: 50,
 });
+
+$('.box3').animate({ 
+    top: 200,
+    left: 30,
+    width: 80,
+    height: 100,
+}).animate({
+    top: 200,
+    left: 50,
+    width: 50,
+    height: 50,
+}).animate({
+    top: 150,
+    left: 300,
+    width: 200,
+    height: 120,
+}).animate({
+    top: 170,
+    left: 170,
+    width: 50,
+    height: 50,
+}).animate({
+    top: 370,
+    left: 150,
+    width: 50,
+    height: 50,
+});
+
+
+
+
+
+/*global windows*/
+/*global currentPos*/
+ /*global math*/
+/*///////////////////////////////////////////////////anime////////////////////////////*/
+
+/*var theThing = document.querySelector("#img1");
+var requestAnimationFrame = windows.requestAnimationFrame;
+
+function moveThing(){
+  currentPos +=5;
+  
+  theThing.style.left = currentPos + "px";
+    if(Math.abs(currentPos) >=900){
+      currentPos = -500;
+    }
+    
+    requestAnimationFrame(moveThing);
+}
+moveThing();*/
+
+/*--------parapara*/
+/*var c = 0;
+    function parapara(){
+        c++;
+        var no = c % 2;
+        document.getElementById('img1').src= no + ".png";
+    }
+    function startAnima() {
+        setInterval("parapara(id)",600);
+    }
+*/    
+function myMove() {
+    var elem = document.getElementById("img1"); 
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++; 
+            elem.style.top = pos + 'px'; 
+            elem.style.left = pos + 'px'; 
+        }
+    }
+}    
+    
